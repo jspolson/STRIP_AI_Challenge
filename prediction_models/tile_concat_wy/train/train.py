cf077f1ac4f7bfdb1bc94f1424bc93728257621b
wenyuan <liwenyuan.zju@gmail.com>
2020-05-13 21:58:01 -0700

    commit train script for training

--- prediction_models/tile_concat_wy/train/train.py
+++ prediction_models/tile_concat_wy/train/train.py
@@ -15,47 +15,49 @@ from sklearn.metrics import cohen_kappa_score,confusion_matrix
 ## custom package
 from input.inputPipeline import *
 from model.resnext_ssl import *
-
-def train_epoch(trainloader, valloader, model, optimizer, criterion):
-    ## train
-    model.train()
-    train_loss = []
-    for i, data in enumerate(tqdm(trainloader), start=0):
-        #             if i >= 50:
-        #                 break
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-        inputs = inputs.cuda()
-        # zero the parameter gradients
-        optimizer.zero_grad()
-        # forward + backward + optimize
-        outputs = model(inputs)
-        loss = criterion(outputs, labels.cuda())
-        train_loss.append(loss.item())
-        loss.backward()
-        optimizer.step()
-    ## val
-    model.eval()
-    val_loss, val_label, val_preds = [], [], []
-    with torch.no_grad():
-        for i, data in enumerate(tqdm(valloader), start=0):
-            #                 if i > 50:
-            #                     break
+class Train(object):
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+    def train_epoch(self,trainloader, valloader, criterion):
+        ## train
+        self.model.train()
+        train_loss = []
+        for i, data in enumerate(tqdm(trainloader, desc='trainIter'), start=0):
+            #             if i >= 50:
+            #                 break
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
             # zero the parameter gradients
-            optimizer.zero_grad()
+            self.optimizer.zero_grad()
             # forward + backward + optimize
             outputs = model(inputs.cuda())
             loss = criterion(outputs, labels.cuda())
-            val_loss.append(loss.item())
-            val_label.append(labels.cpu())
-            val_preds.append(outputs.cpu())
-    # scheduler.step()
-    val_preds = torch.argmax(torch.cat(val_preds, 0), 1)
-    val_label = torch.cat(val_label)
-    kappa = cohen_kappa_score(val_label, val_preds, weights='quadratic')
-    return np.mean(train_loss), np.mean(val_loss), kappa
+            train_loss.append(loss.item())
+            loss.backward()
+            self.optimizer.step()
+        ## val
+        model.eval()
+        val_loss, val_label, val_preds = [], [], []
+        with torch.no_grad():
+            for i, data in enumerate(tqdm(valloader, desc='valIter'), start=0):
+                #                 if i > 50:
+                #                     break
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                # forward + backward + optimize
+                outputs = model(inputs.cuda())
+                loss = criterion(outputs, labels.cuda())
+                val_loss.append(loss.item())
+                val_label.append(labels.cpu())
+                val_preds.append(outputs.cpu())
+        # scheduler.step()
+        val_preds = torch.argmax(torch.cat(val_preds, 0), 1)
+        val_label = torch.cat(val_label)
+        kappa = cohen_kappa_score(val_label, val_preds, weights='quadratic')
+        return np.mean(train_loss), np.mean(val_loss), kappa, optimizer
 
 if __name__ == "__main__":
     nfolds = 5
@@ -77,11 +79,12 @@ if __name__ == "__main__":
     model = Model().cuda()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=0)
+    Training = Train(model, optimizer)
     for fold in trange(nfolds, desc='fold'):
         trainloader, valloader = crossValData(fold)
         for epoch in trange(epochs, desc='epoch'):
-            train_loss, val_loss, kappa = train_epoch(trainloader,valloader,model,optimizer,criterion)
-            print("Epoch {}, train loss: {:.4f}, val loss: {:.4f}, kappa-score: {:.4f}".format(epoch,
+            train_loss, val_loss, kappa = Training.train_epoch(trainloader,valloader,criterion)
+            print("Epoch {}, train loss: {:.4f}, val loss: {:.4f}, kappa-score: {:.4f}.\n".format(epoch,
                                                                                                train_loss,
                                                                                                val_loss,
                                                                                                kappa))
