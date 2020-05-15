13b2b8850f49cfda16936ed4b83383184ecf97fe
wenyuan <liwenyuan.zju@gmail.com>
2020-05-15 12:16:49 -0700

    commit tensorboard tracking and model saving

--- prediction_models/tile_concat_wy/train/train.py
+++ prediction_models/tile_concat_wy/train/train.py
@@ -71,7 +71,7 @@ def save_checkpoint(state, is_best, fname):
         shutil.copyfile('{}_ckpt.pth.tar'.format(fname), '{}_best.pth.tar'.format(fname))
 
 if __name__ == "__main__":
-    fname = "Resnext50_Adam_NoScheduler"
+    fname = "Resnext50_30epoch"
     nfolds = 5
     bs = 32
     epochs = 30
