b501c6c553ea9a4c49f0117254baf6c126ae38a8
wenyuan <liwenyuan.zju@gmail.com>
2020-05-18 19:25:22 -0700

    commit regression model

--- prediction_models/tile_concat_wy/train/train.py
+++ prediction_models/tile_concat_wy/train/train.py
@@ -74,10 +74,10 @@ def save_checkpoint(state, is_best, fname):
         torch.save(state.pop('optimizer'), '{}_best.pth.tar'.format(fname)) ## only save weights for best model
 
 if __name__ == "__main__":
-    fname = "Resnext50_reg_epoch30"
+    fname = "Resnext50_reg_scheduler"
     nfolds = 4
     bs = 32
-    epochs = 30
+    epochs = 16
     csv_file = '../input/panda-16x128x128-tiles-data/{}_fold_train.csv'.format(nfolds)
     image_dir = '../input/panda-16x128x128-tiles-data/train/'
     ## image statistics
@@ -110,7 +110,7 @@ if __name__ == "__main__":
         # scheduler = optim.lr_scheduler.StepLR(optimizer, 1, 1)
         optimizer = Over9000(model.parameters())
         scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr = 1e-3, total_steps = epochs,
-                                                  pct_start = 0.3, div_factor = 100)
+                                                  pct_start = 0.0, div_factor = 100)
         Training = Train(model, optimizer, scheduler)
         best_kappa = 0
         weightsPath = os.path.join(weightsDir, '{}_{}'.format(fname, fold))
