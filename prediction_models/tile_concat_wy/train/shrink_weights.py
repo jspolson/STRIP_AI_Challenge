9657d98c9ab8001ef299bc14c2b4ac9458bbf8cc
wenyuan <liwenyuan.zju@gmail.com>
2020-05-18 21:47:18 -0700

    commit shrink the model

--- prediction_models/tile_concat_wy/train/shrink_weights.py
+++ prediction_models/tile_concat_wy/train/shrink_weights.py
@@ -1,6 +1,6 @@
 import torch
-weights = [f'./weights/Resnext50_reg/Resnext50_reg_{i}_best.pth.tar' for i in range(5)]
+weights = [f'./weights/Resnext50_reg/Resnext50_reg_{i}_ckpt.pth.tar' for i in range(5)]
 for i in range(5):
     state_dict = torch.load(weights[i])
     weights_state = state_dict['state_dict']
-    torch.save(weights_state, f'./weights/Resnext50_reg/Resnext50_reg_{i}_little.pth')
+    torch.save(weights_state, f'./weights/Resnext50_reg/Resnext50_reg_{i}_best.pth')
